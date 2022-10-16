989702da510c722fd6d4c796db5acd75116becea
Coder-wizard <dev.nayeem.sheikh@gmail.com>
2022-10-16 07:17:20 +0600

    google authadded

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -7,6 +8,11 @@ export const authOptions = {
     GithubProvider({
       clientId: "6a850ef8356e9aed0449",
       clientSecret: "e7a49ec111760634583be68a0a760e55e4f45b5b",
+    }),
+    GoogleProvider({
+      clientId:
+        "230724927544-t6cd0i643fmee918djqovejsq10h06se.apps.googleusercontent.com",
+      clientSecret: "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",
     }),
     // ...add more providers here
   ],
